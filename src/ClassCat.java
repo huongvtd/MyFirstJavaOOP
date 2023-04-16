2a7195965d44e2a5d49cf5ca25454d367fb38fb5
huong <huongvtd@gmail.com>
2023-04-16 11:15:37 +0700

    16042023

--- src/ClassCat.java
+++ src/ClassCat.java
@@ -1,11 +1,23 @@
 public class ClassCat {
+
+    private String name;
+    private String breed;
+
     public ClassCat(String name, String breed) {
         this.name = name;
         this.breed = breed;
     }
+    public String getName() {
+        return name;
+    }
 
-    private String name;
-    private String breed;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        ClassDemo instance = new ClassDemo();
+       /* ClassDemo instance = new ClassDemo();
         instance.number = 15;
         instance.words = "New imagination";
         instance.printNumber();
         instance.printWords();
 
         ClassDemo instance2 = new ClassDemo();
-        instance2.printNumber();
+        instance2.printNumber();*/
 
        /* ClassLamp kitchen = new ClassLamp("Kitchen");
         ClassLamp bedroom = new ClassLamp("Bedroom");
@@ -21,5 +21,9 @@ public class Main {
      /*   ClassPrograming instance = new ClassPrograming("Java");
         instance.completeLearning();
         instance.learningStatus();*/
+
+        ClassCat cat = new ClassCat("Jerry", "Vietnamese black");
+        cat.setName("Suri");
+        System.out.println("I have a cat named "+ cat.getName() + " who is " + cat.getBreed() + ".");
     }
 }
